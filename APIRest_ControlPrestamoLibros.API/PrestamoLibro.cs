8024652757f9c39a74df8786f5af1afb5d2e0748
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2023-08-03 11:34:56 -0500

    Add model

---
+++ APIRest_ControlPrestamoLibros.API/PrestamoLibro.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIRest_ControlPrestamoLibros.API
+{
+    public class PrestamoLibro
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid Isbn { get; set; }
+        public string IdentificacionUsuario { get; set; } = string.Empty;
+        public int TipoUsuario { get; set; }
+        public DateTime FechaMaximaDevolucion { get; set; }
+
+    }
+}
